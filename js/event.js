79da3a1a003a8384137d38729357676e1953d38a
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-02-08 20:57:14 +0600

    new file

--- js/event.js
+++ js/event.js
@@ -15,3 +15,39 @@ blueButton.onclick = makeBlue;
 function makeBlue() {
     document.body.style.backgroundColor = 'blue';
 }
+
+
+// handle event using anonymous function-----fourth way
+
+const greenButton = document.getElementById('green-button');
+// greenButton.onclick = function makeGreen() {
+
+// anonymous function
+greenButton.onclick = function () {
+    document.body.style.backgroundColor = 'green';
+}
+
+
+// handle event using eventlistener  ---fifth
+
+const goldenRod = document.getElementById('golden-rod');
+goldenRod.addEventListener('click', makeGoldenRoad);
+
+function makeGoldenRoad() {
+    document.body.style.backgroundColor = 'goldenrod';
+}
+
+
+// add event listener       ---sixth
+const hotPink = document.getElementById('hot-pink');
+console.log(hotPink);
+hotPink.addEventListener('click', function () {
+    document.body.style.backgroundColor = 'hotpink';
+})
+
+
+// add event listener ---seven
+
+document.getElementById('light-blue').addEventListener('click', function () {
+    document.body.style.backgroundColor = 'lightblue';
+})
